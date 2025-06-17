4d60b316cd56ec39441821e6853ddc8783aa32e3
kiran nitawadekar <kirannitawadekar@gmail.com>
2025-06-17 13:46:26 +0530

    add humburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,28 @@
-import {Link} from 'react-router-dom'
-import './Header.css'
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+import './Header.css';
 
 const Header =() =>{
+    const [isOpen, setMenuOpen]=useState(false);
+    const toggleMenu = () => {setMenuOpen(!isOpen);};
 
     return(
         <header>
             <nav className='navbar'>
-                <Link className='f'>Vivekanand College</Link>
-                <Link className='f'to='/'>Home</Link>
-                <Link to='/admissionspage'></Link>
-                <Link className='f'to='/aboutpage'>About</Link>
-                <Link className='f'to='/coursespage'>Course</Link>
-                <Link className='f'to='/contactpage'>Contact</Link>
-                <Link className='f'to='/admissionspage'><button className='but'><u>Apply Now!</u></button></Link>
-            </nav>
-        </header>
-    )
-}
+            <h2 style={{color:"white"}}>Vivekanand College</h2>
+                <div className={`nav-links ${isOpen ? 'open':''}`}>
+                    <Link className='f'to='/'>Home</Link>
+                    <Link className='f'to='/aboutpage'>About</Link>
+                    <Link className='f'to='/coursespage'>Course</Link>
+                    <Link className='f'to='/contactpage'>Contact</Link>
+                     <Link to='/admissionspage'></Link>
+                </div>
+                    <div className='humburger'onClick={toggleMenu}>&#9776;</div>
+                    <Link className='f'to='/admissionspage'><button className='apply-btn'><u>Apply Now!</u></button></Link>
+            </nav>    
+         </header>
+    );
+};
 
-export default Header
+export default Header;
 
